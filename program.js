39463a4fc3adba1d77aff309e741726a5dbf6fe7
IgrisUVG <igris@ushivangoga.ru>
2017-10-03 12:56:11 +0300

    learnyounode 4

--- program.js
+++ program.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
-var buf = fs.readFileSync(process.argv[2]);
-var str = buf.toString().split('\n').length - 1;
-
-console.log(str);
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data.split('\n').length - 1);
+    }
+});
